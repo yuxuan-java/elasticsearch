1b02ed2e368b1001b876af40b37528cf6a9aaf89
yuxuan.han <hanyuxuan@youhaodongxi.com>
2018-12-10 16:33:07 +0800

    add IESUserInfoService

--- src/main/java/com/example/elasticsearch/ESUserInfoService.java
+++ src/main/java/com/example/elasticsearch/ESUserInfoService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-public class ESUserInfoService {
+public class ESUserInfoService implements IESUserInfoService {
     
     @Autowired
     private ESUserInfoRepository esUserInfoRepository;
---
+++ src/main/java/com/example/elasticsearch/IESUserInfoService.java
@@ -0,0 +1,4 @@
+package com.example.elasticsearch;
+
+public interface IESUserInfoService {
+}
